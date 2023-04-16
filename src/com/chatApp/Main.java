a70501b1e8e1fb36736f62ef6be75ed2ef3be1db
seveneleven337 <solidxnake_6@hotmail.com>
2023-04-16 11:22:49 +0300

    init

---
+++ src/com/chatApp/Main.java
@@ -0,0 +1,36 @@
+package com.chatApp;
+
+import java.io.*;
+import java.net.*;
+
+class Server {
+    public static void main(String[] args)
+    {
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(6000);
+            server.setReuseAddress(true);
+            while (true) {
+                Socket client = server.accept();
+                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
+                ClientHandler clientSock = new ClientHandler(client);
+                new Thread(clientSock).start();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (server != null) {
+                try {
+                    server.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}
